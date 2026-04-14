1a5d61c73d8e38361e0a651746980c91475bb9d8
Adrian Agresta <adrian.agresta@outlook.com>
2026-04-14 17:03:37 -0500

    Scaffolded first table from database using entity framework.

---
+++ API/Models/Application.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API.Models;
+
+public partial class Application
+{
+    public int Id { get; set; }
+
+    public string Company { get; set; } = null!;
+
+    public DateOnly? Applydate { get; set; }
+
+    public DateOnly? Rejectdate { get; set; }
+
+    public int? Salarymin { get; set; }
+
+    public int? Salarymax { get; set; }
+
+    public string? Link { get; set; }
+
+    public bool Applied { get; set; }
+
+    public bool Rejected { get; set; }
+
+    public string? Location { get; set; }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true)
+    .AddJsonFile("appsettings.Local.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables();
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//builder.Services.AddDbContext<JatfniContext>(options =>
+//    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
